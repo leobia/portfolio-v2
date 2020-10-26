7eeb10ed5a280d03dcde1e13cf897186c2c50475
Leonardo Bianco <leonardo.bianco@outlook.com>
2020-10-26 07:56:07 +0100

    :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <input type="checkbox" class="theme-switch" v-model="darkMode" />
+  </div>
+
+  <router-view class="view-container" />
+</template>
+
+<style lang="scss"></style>
+
+<script>
+export default {
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  mounted() {
+    // set 'app-background' class to body
+    let bodyElement = document.body
+    bodyElement.classList.add('app-background')
+
+    let htmlElement = document.documentElement
+    let theme = localStorage.getItem('theme')
+
+    if (theme === 'dark') {
+      htmlElement.setAttribute('theme', 'dark')
+      this.darkMode = true
+    } else {
+      htmlElement.setAttribute('theme', 'light')
+      this.darkMode = false
+    }
+  },
+  watch: {
+    darkMode: function() {
+      // add/remove class to/from html tag
+      let htmlElement = document.documentElement
+
+      if (this.darkMode) {
+        localStorage.setItem('theme', 'dark')
+        htmlElement.setAttribute('theme', 'dark')
+      } else {
+        localStorage.setItem('theme', 'light')
+        htmlElement.setAttribute('theme', 'light')
+      }
+    }
+  }
+}
+</script>
