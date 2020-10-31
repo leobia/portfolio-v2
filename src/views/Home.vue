8fd9287c4a76c9eac547195d1a71186284c95b0c
Leonardo Bianco <leonardo.bianco@outlook.com>
2020-10-31 16:49:05 +0100

    :feat: multiple changes
    - Removed container and style sub elements directly
    - migrated all scss file to singular name
    - added sections with diagonal background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="home-container">home</div>
+  <div class="home-container">
+    <section>
+      <div class="section-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam modi
+        adipisci vel ipsum ab.
+      </div>
+    </section>
+    <section>
+      <div class="section-content">
+        Aliquid alias non error culpa dicta veritatis suscipit odio neque autem
+        sequi nostrum iure, debitis voluptas vero delectus dignissimos
+        perferendis sit magnam asperiores, repudiandae sint tempora tempore
+        commodi omnis. Exercitationem.
+      </div>
+    </section>
+    <section>
+      <div class="section-content">
+        Magni repellendus dolor, magnam earum laboriosam ab est distinctio in id
+        corporis, porro temporibus. Explicabo necessitatibus cumque, earum rem
+        provident impedit! Dicta doloribus assumenda ipsam rerum! Placeat
+        aliquam tenetur tempore.
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
