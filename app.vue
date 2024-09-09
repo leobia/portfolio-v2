5ae0505fdc7405c65c8f863dfef478db9b65de73
Leonardo Bianco <leonardo.bianco@outlook.com>
2024-09-09 10:38:05 +0200

    Added download of cv

--- app.vue
+++ app.vue
@@ -9,7 +9,11 @@
         <header class="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
           <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl"><a href="/">Leonardo Bianco</a></h1>
           <h2 class="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Software Developer</h2>
-          <p class="mt-4 max-w-xs leading-normal">I build enterprise web applications.
+          <p class="mt-4 max-w-xs leading-normal">I build enterprise web applications. You can find my italian CV <a
+              class="inline-flex items-baseline leading-tight underline text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link"
+              href="/files/cv_leonardo_bianco_it.pdf" download target="_blank" rel="noreferrer noopener">
+              here
+            </a>
           </p>
           <NavLinks />
           <SocialIcons />
